289cc7243c64301e115b87d81fe112fde4b2e4b2
João Carlos <joaocarloslima@me.com>
2022-03-08 12:36:01 -0300

    :tada: primeiro programa

---
+++ PrimeiroPrograma/src/PrimeiroPrograma.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class PrimeiroPrograma {
+	
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Olá Mundo");
+	}
+	
+}
+
+// ctrl + f11
